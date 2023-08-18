9f85364b0a9f45476ed1b9399b385d422b570774
linkinghack <linking@linkinghack.com>
2023-08-18 03:30:05 +0000

    customize start hint condition

--- src/common-utils/scripts/rc_snippet.sh
+++ src/common-utils/scripts/rc_snippet.sh
@@ -3,11 +3,11 @@ if [ -z "${USER}" ]; then export USER=$(whoami); fi
 if [[ "${PATH}" != *"$HOME/.local/bin"* ]]; then export PATH="${PATH}:$HOME/.local/bin"; fi
 
 # Display optional first run image specific notice if configured and terminal is interactive
-if [ -t 1 ] && [[ "${TERM_PROGRAM}" = "vscode" || "${TERM_PROGRAM}" = "codespaces" ]] && [ ! -f "$HOME/.config/vscode-dev-containers/first-run-notice-already-displayed" ]; then
+if [ -t 1 ] && [ ! -f "$HOME/.config/vscode-dev-containers/first-run-notice-already-displayed" ]; then
     if [ -f "/usr/local/etc/vscode-dev-containers/first-run-notice.txt" ]; then
         cat "/usr/local/etc/vscode-dev-containers/first-run-notice.txt"
-    elif [ -f "/workspaces/.codespaces/shared/first-run-notice.txt" ]; then
-        cat "/workspaces/.codespaces/shared/first-run-notice.txt"
+    elif [ -f "/workspaces/.devspace/shared/first-run-notice.txt" ]; then
+        cat "/workspaces/.devspace/shared/first-run-notice.txt"
     fi
     mkdir -p "$HOME/.config/vscode-dev-containers"
     # Mark first run notice as displayed after 10s to avoid problems with fast terminal refreshes hiding it
